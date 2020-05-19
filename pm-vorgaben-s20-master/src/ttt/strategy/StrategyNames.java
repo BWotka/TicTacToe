07f340f7b9f62ddd1249c45a4ecf038b6e8b06d6
bwotka <b.wotka@mail.de>
2020-05-19 12:49:27 +0200

    Enum Strategien

---
+++ pm-vorgaben-s20-master/src/ttt/strategy/StrategyNames.java
@@ -0,0 +1,12 @@
+
+package ttt.strategy;
+
+/**
+ * Enum that holds all the different strategies.
+ * 
+ * @author Benedikt Wotka, David Nickel
+ *
+ */
+public enum StrategyNames {
+  NoEasyLose(), StrictOrder(), HumanPlay(), MinMaxStrategy()
+}
