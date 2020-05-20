94278a3012861c4338b5716fc8bfc66e122b92eb
bwotka <b.wotka@mail.de>
2020-05-20 13:46:37 +0200

    Code kommentiert

--- pm-vorgaben-s20-master/src/ttt/game/Starter.java
+++ pm-vorgaben-s20-master/src/ttt/game/Starter.java
@@ -1,11 +1,11 @@
 
 package ttt.game;
 
-import charts.Charts;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import charts.Charts;
 import ttt.strategy.HumanPlay;
-import ttt.strategy.MinMaxStrategy;
+import ttt.strategy.StrictOrder;
 
 /**
  * Starter class, sets up the game.
@@ -22,29 +22,35 @@ public class Starter {
    */
   public static void main(String[] args) {
 
+    // setup of the reader for strat human play
     InputStreamReader inread = new InputStreamReader(System.in);
     BufferedReader reader = new BufferedReader(inread);
 
+    // setup of two players. one with strat human play, the other one using strictorder
     Player xplayer = new Player('X');
     xplayer.setStrategy(new HumanPlay(reader));
     Player oplayer = new Player('O');
-    oplayer.setStrategy(new MinMaxStrategy());
+    oplayer.setStrategy(new StrictOrder());
 
+    // creating the Game instance
     Game maingame = new Game();
 
 
-
+    // setting the players in the game
     maingame.setPlayerX(xplayer);
     maingame.setPlayerO(oplayer);
 
+    // starting the game
     maingame.playGame();
 
+    // showing stats fir all games until now
     Charts chart = new Charts();
     chart.saveStats(maingame.playGame(), xplayer, oplayer);
     chart.chartPlayer(xplayer);
     chart.chartPlayer(oplayer);
     Player[] players = {xplayer, oplayer};
     chart.chartPlayers(players);
+
     chart.chartGames(xplayer, oplayer);
 
 
